1865b1158ea842fa205434100a79656110369d76
Quertier Maxime <119400854+F0rline@users.noreply.github.com>
2023-01-06 11:48:14 +0100

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,50 @@
 const btnBox = document.getElementById('box');
+const btnLessBox = document.getElementById('box-less');
+const bioBox = document.getElementById('biobox');
+btnLessBox.style.display = 'none'
 
 btnBox.addEventListener('click', () => {
   btnBox.style.display = 'none';
 
-  const bioBox = document.getElementById('biobox');
   bioBox.style.display = 'block';
+
+  btnLessBox.style.display = 'block';
+
 });
+
+btnLessBox.addEventListener('click' , () => {
+  btnBox.style.display = 'block';
+
+  bioBox.style.display = 'none';
+
+  btnLessBox.style.display = 'none';
+
+})
+
+
+
+
+
+
+const btnMoreAlbum = document.getElementById('more-album');
+const btnLessAlbum = document.getElementById('less-album');
+const container = document.getElementById('add-album');
+btnLessAlbum.style.display = 'none'
+
+btnMoreAlbum.addEventListener('click', () => {
+  btnMoreAlbum.style.display = 'none';
+
+  btnLessAlbum.style.display = 'block';
+
+  container.style.display = 'block';
+
+});
+
+btnLessAlbum.addEventListener('click' , () => {
+  btnMoreAlbum.style.display = 'block';
+
+  container.style.display = 'none';
+
+  btnLessAlbum.style.display = 'none';
+
+})
